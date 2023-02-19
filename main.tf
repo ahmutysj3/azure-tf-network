9247151eefc913c19cfbfd9121b1242d14abbfc4
trace <ahmutysj3@gmail.com>
2023-02-19 15:14:23 -0500

    added vnet peering

--- main.tf
+++ main.tf
@@ -20,8 +20,26 @@ resource "azurerm_virtual_network" "trace" {
   address_space       = [cidrsubnet("${var.supernet}",8,each.value)]
 
   tags = {
-    environment = "Trace-AZ-Lab"
+    environment = "Trace_AZ_Lab"
   }
+}
+
+resource "azurerm_virtual_network_peering" "hub_dmz" {
+  name                      = "vnet_peering_hub_to_dmz"
+  resource_group_name       = azurerm_resource_group.trace.name
+  virtual_network_name      = azurerm_virtual_network.trace["hub"].name
+  remote_virtual_network_id = azurerm_virtual_network.trace["dmz"].id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
+
+resource "azurerm_virtual_network_peering" "dmz_hub" {
+  name                      = "vnet_peering_dmz_to_hub"
+  resource_group_name       = azurerm_resource_group.trace.name
+  virtual_network_name      = azurerm_virtual_network.trace["dmz"].name
+  remote_virtual_network_id = azurerm_virtual_network.trace["hub"].id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
 }
 
 # creates 2 x /25 subnets for inside and outside fw interfaces
@@ -34,6 +52,23 @@ resource "azurerm_subnet" "hub" {
   resource_group_name = azurerm_resource_group.trace.name
   virtual_network_name = azurerm_virtual_network.trace["hub"].name
   address_prefixes = [each.value]
+}
+
+resource "azurerm_route_table" "outside" {
+  name                          = "hub_outside_rt"
+  location                      = azurerm_resource_group.trace.location
+  resource_group_name           = azurerm_resource_group.trace.name
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "route1"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "Internet"
+  }
+
+  tags = {
+    environment = "Trace_AZ_Lab"
+  }
 }
 
 # creates a spoke subnet for each entry in var.subnet_params and assigns to vnet listed in "vnet" argument
