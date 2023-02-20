6188d3003519476ffb7040fd46f73276996d0935
trace <ahmutysj3@gmail.com>
2023-02-19 23:28:05 -0500

    tf fmt and added deny all out

--- main.tf
+++ main.tf
@@ -177,6 +177,7 @@ resource "azurerm_network_security_rule" "spokes_to_hub" {
   network_security_group_name = azurerm_network_security_group.spokes[each.key].name
 }
 
+
 resource "azurerm_network_security_rule" "dmz_outbound_to_app" {
   name                        = "dmz_out_to_app"
   priority                    = 101
@@ -276,11 +277,41 @@ resource "azurerm_network_security_rule" "default_allow_all_internal_vnet" {
   network_security_group_name = azurerm_network_security_group.spokes[each.key].name
 }
 
+resource "azurerm_network_security_rule" "default_allow_out_internal_vnet" {
+  for_each                    = { for k, v in azurerm_virtual_network.trace : k => v if k != "hub" }
+  name                        = "default_allow_out_internal_vnet"
+  priority                    = 201
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = element(azurerm_virtual_network.trace[each.key].address_space, 0)
+  destination_address_prefix  = element(azurerm_virtual_network.trace[each.key].address_space, 0)
+  resource_group_name         = azurerm_resource_group.network.name
+  network_security_group_name = azurerm_network_security_group.spokes[each.key].name
+}
+
 resource "azurerm_network_security_rule" "default_deny_all_in" {
   for_each                    = { for k, v in azurerm_virtual_network.trace : k => v if k != "hub" }
   name                        = "default_deny_all_in"
   priority                    = 4096
   direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.network.name
+  network_security_group_name = azurerm_network_security_group.spokes[each.key].name
+}
+
+resource "azurerm_network_security_rule" "default_deny_all_out" {
+  for_each                    = { for k, v in azurerm_virtual_network.trace : k => v if k != "hub" }
+  name                        = "default_deny_all_out"
+  priority                    = 4096
+  direction                   = "Outbound"
   access                      = "Deny"
   protocol                    = "*"
   source_port_range           = "*"
