1e579027cc6eee5d064c7d1b2621ca69ed26778c
Trace Ahmuty <ahmutysj3@gmail.com>
2023-02-20 23:19:54 -0500

    changed dir structure

--- example-root-module/main.tf
+++
@@ -1,7 +0,0 @@
-module "network" {
-  source = "github.com/ahmutysj3/trace-azure-tf"
-  network_name = var.network_name
-  supernet = var.supernet
-  vnet_params = var.vnet_params
-  subnet_params = var.subnet_params
-}
