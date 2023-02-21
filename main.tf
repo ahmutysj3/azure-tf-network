d6bb83be6213174ed30a741365e94ffefe0842f5
Trace Ahmuty <ahmutysj3@gmail.com>
2023-02-20 23:50:30 -0500

    asdf

--- main.tf
+++ main.tf
@@ -32,23 +32,3 @@ output "spokes_route_tables" {
 }
 
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.36.0"
-    }
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.35.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "trace-tf-unlocked-bucket"
-    key    = "network/azure-terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
