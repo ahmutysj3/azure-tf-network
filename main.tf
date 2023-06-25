6641947ca48597a89b1010694a76b91dc1cd1cae
Trace Ahmuty <ahmutysj3@gmail.com>
2023-06-25 18:34:18 -0400

    hub side of vnet peerings added

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "azurerm_subnet" "spoke_2" {
   address_prefixes     = ["10.2.0.0/24"]
 }
 
-resource "azurerm_virtual_network_peering" "spoke_1" {
+resource "azurerm_virtual_network_peering" "spoke_1_to_hub" {
   name                      = "spoke_1_to_hub"
   resource_group_name       = data.azurerm_resource_group.tf_lab.name
   virtual_network_name      = azurerm_virtual_network.spoke_1.name
@@ -74,7 +74,7 @@ resource "azurerm_virtual_network_peering" "spoke_1" {
   }
 }
 
-resource "azurerm_virtual_network_peering" "spoke_2" {
+resource "azurerm_virtual_network_peering" "spoke_2_to_hub" {
   name                      = "spoke_2_to_hub"
   resource_group_name       = data.azurerm_resource_group.tf_lab.name
   virtual_network_name      = azurerm_virtual_network.spoke_2.name
@@ -82,5 +82,27 @@ resource "azurerm_virtual_network_peering" "spoke_2" {
 
   triggers = {
     remote_address_space = join(",", azurerm_virtual_network.security.address_space)
+  }
+}
+
+resource "azurerm_virtual_network_peering" "hub_to_spoke_1" {
+  name                      = "hub_to_spoke_1"
+  resource_group_name       = data.azurerm_resource_group.tf_lab.name
+  virtual_network_name      = azurerm_virtual_network.security.name
+  remote_virtual_network_id = azurerm_virtual_network.spoke_1.id
+
+  triggers = {
+    remote_address_space = join(",", azurerm_virtual_network.spoke_1.address_space)
+  }
+}
+
+resource "azurerm_virtual_network_peering" "hub_to_spoke_2" {
+  name                      = "hub_to_spoke_2"
+  resource_group_name       = data.azurerm_resource_group.tf_lab.name
+  virtual_network_name      = azurerm_virtual_network.security.name
+  remote_virtual_network_id = azurerm_virtual_network.spoke_2.id
+
+  triggers = {
+    remote_address_space = join(",", azurerm_virtual_network.spoke_2.address_space)
   }
 }
